46d4cc6b8750b83ea0992b0fc4406a6c2fad35d7
Cian Twyford <c16380866@mydit.ie>
2018-02-14 16:23:12 +0000

    Class and Fields

---
+++ JavaTest.java
@@ -0,0 +1,20 @@
+package ie.dit;
+
+public class Tune {
+	
+	private static int x() {
+		
+	}
+	
+	private static String title() {
+		
+	}
+	
+	private static String altTitle() {
+		
+	}
+	
+	private static String notation() {
+		
+	}
+}
