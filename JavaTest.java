ddf00fae9b746611b087eca2472a4ec01d1c27d2
Cian Twyford <c16380866@mydit.ie>
2018-02-14 16:33:41 +0000

    toString

--- JavaTest.java
+++ JavaTest.java
@@ -1,6 +1,22 @@
 package ie.dit;
+import java.util.ArrayList;
 
 public class Tune {
+	
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		for (String word:words)
+		{
+			if(altTitle != null) {
+				sb.append(x + ", " + title + ", " + altTitle);
+			}
+			else {
+				sb.append(x + ", " + title + ", ");
+			}
+		}
+		
+		return sb.toString();
+	}
 	
 	private static int x() {
 		
